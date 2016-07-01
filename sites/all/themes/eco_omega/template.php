3f77bab4a942a53c6f8cf0814819e491f6914cdb
Anthony Fenwick <anthonyfenwick.1@gmail.com>
2016-07-01 10:18:22 +0100

    Web form styles, remove sass debug

--- sites/all/themes/eco_omega/template.php
+++ sites/all/themes/eco_omega/template.php
@@ -5,3 +5,7 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * Eco Omega theme.
  */
+
+function eco_omega_commerce_cart_empty_block() {
+	return '<div class="cart-empty-block"></div>';
+}
--- sites/all/themes/eco_omega/templates/template.php
+++ sites/all/themes/eco_omega/templates/template.php
@@ -6,4 +6,3 @@
  * Eco Omega theme.
  */
 
-
