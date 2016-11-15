57d43ab8f2d82d9da89b30acbf949f8bc378bba3
Anthony Fenwick <anthonyfenwick.1@gmail.com>
2016-11-15 15:55:30 +0000

    Get control of the Devel Krumo images

--- sites/all/themes/eco_omega/template.php
+++ sites/all/themes/eco_omega/template.php
@@ -6,6 +6,12 @@
  * Eco Omega theme.
  */
 
+//Lets hide the cart when its empty
 function eco_omega_commerce_cart_empty_block() {
 	return '<div class="cart-empty-block"></div>';
 }
+
+//Lets work with the Devel Module and see what arrays are printed?
+//function eco_omega_preprocess_node(&$vars) {
+//	kpr($vars);
+//}
